34ccc474c1bf105d7e58caa2c3484cda00008c90
Bibek Thapa <75052457+B-KEY@users.noreply.github.com>
2024-11-14 17:13:02 +0000

    Update page.jsx

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -17,7 +17,8 @@ const Blog = () => {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const res = await fetch(`${DEV_TO_CONFIG.API_URL}/articles?username=bibek-thapa01`);
+        const username = 'bibek-thapa01'; 
+        const res = await fetch(`${DEV_TO_CONFIG.API_URL}/articles?username=${username}`);
         const data = await res.json();
         
         const sortedBlogs = data.sort((a, b) => 
