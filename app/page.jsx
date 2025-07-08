b7983f626836ece5404a416bd0510fa9f7160910
Bibek Thapa <75052457+B-KEY@users.noreply.github.com>
2025-07-08 12:48:25 +0100

    Update page.jsx
    update cv

--- app/page.jsx
+++ app/page.jsx
@@ -49,7 +49,7 @@ const Home = () => {
             </div>
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <a href="https://drive.google.com/file/d/1ip3mwXUrYBVOk6364jZ45eYMS557r0Hw/view?usp=sharing" download>
+              <a href="https://drive.google.com/file/d/1DV68loaZVf45XvoPILUDkJcrp_cr9xNP/view?usp=sharing" download>
                 <Button
                   variant="outline"
                   size="lg"
